0986be87f236d3f9ee3d691e4cac021e3bed8432
peinnagato <tamangraaz78@gmail.com>
2025-03-12 17:14:11 +0545

    Adding theme template

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MainController;
 
 Route::get('/', function () {
     return view('welcome');
 });
- Route::view('/contact','welcome');
+Route::get('/products', function () {
+    return view('products');
+});
+
+ Route::get('/index',[MainController::class,'index']);
