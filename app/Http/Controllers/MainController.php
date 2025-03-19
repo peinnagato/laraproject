b81d9ac6f594d46b89b330166ebb95eefa25a346
peinnagato <tamangraaz78@gmail.com>
2025-03-19 22:00:22 +0545

    Route and blade template

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class MainController extends Controller
 {
     public function index(){
-        return view('index');
+        return view('about');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,23 @@ Route::get('/products', function () {
 });
 
  Route::get('/index',[MainController::class,'index']);
+
+ Route::get('/raj-tamang',function(){
+    return view('contact');
+ });
+ Route::get('/admin/index', function(){
+    return view('admin/index');
+ });
+ Route::prefix('admin')->group(function(){
+
+    Route::get('/index', function(){
+        return view('admin.index');
+     });
+     Route::get('/flag', function(){
+        return view('admin.flag');
+     });
+ });
+ Route::fallback(function(){
+    return "<h1>Page not Found </h1>";
+
+ });
